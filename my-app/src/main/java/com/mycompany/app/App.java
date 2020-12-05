58db972bcbbf0a2994562c47a83e3a8b4d145bcc
maria <maria.morra@studenti.unipd.it>
2020-12-05 18:49:55 +0100

    first setup

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// [EGON] [GALVANI] [1187021]
+////////////////////////////////////////////////////////////////////
+
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
